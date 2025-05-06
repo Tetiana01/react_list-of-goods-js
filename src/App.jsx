1f78d88287d2333841d63afb7074b293604108ce
Tanya <gusievatania2004@gmail.com>
2025-05-06 15:00:57 +0300

    fix: adding key

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,9 @@ export const App = () => {
 
       <ul>
         {sortedGoods.map(good => (
-          <li data-cy="Good">{good}</li>
+          <li data-cy="Good" key={good}>
+            {good}
+          </li>
         ))}
       </ul>
     </div>
